184a9fae72c2187561c34f39e7f2f356df341ac7
megatontech <techme@live.cn>
2020-06-01 21:44:09 +0800

    依赖注入

--- src/Presentation/SmartStore.Web.Framework/DependencyRegistrar.cs
+++ src/Presentation/SmartStore.Web.Framework/DependencyRegistrar.cs
@@ -170,6 +170,7 @@ namespace SmartStore.Web.Framework
 				.InstancePerRequest();
 
 			builder.RegisterType<ProductService>().As<IProductService>().InstancePerRequest();
+			builder.RegisterType<DeclarationProductService>().As<IDeclarationProductService>().InstancePerRequest();
 
 			builder.RegisterType<BackInStockSubscriptionService>().As<IBackInStockSubscriptionService>().InstancePerRequest();
 			builder.RegisterType<CompareProductsService>().As<ICompareProductsService>().InstancePerRequest();
